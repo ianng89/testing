5babb9119220a54aea3578102adee6280d4ab550
ianng89 <ianng@Ians-MacBook-Pro.local>
2015-12-17 19:55:18 +0800

    im done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,28 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+validates :email, :format => { :with => /\w+[@]\w+[.]\w{1}\w+/}, uniqueness: { case_sensitive: false }
+validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+validate :validate_phone	
+
+	def name
+
+	"#{first_name} #{last_name}"
+
+	end
+
+	 def validate_phone
+	        phone_num = phone.gsub(/[^\d]/, '')
+	        if phone_num.length < 10
+	            errors.add(:phone,"invalid phone number.")
+	    	end
+	 end
+	def age
+		now = Date.today
+		age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+	end
 end
+# SELECT * FROM student 
+# WHERE first_name = #{first_name}
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,16 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+  	create_table :students do |t|
+      t.string :first_name, length: 64
+      t.string :last_name, length: 64
+      t.string :gender, length: 64
+      t.date :birthday
+      t.text :email, length:64
+      t.string :phone, length:10
+      t.timestamps
+
+  	end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
